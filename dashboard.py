730abeb0d90b78305220599c6f2d484ce1ffd0f8
Weber <andweber92@gmail.com>
2024-04-07 12:36:42 -0300

    adjusts in initial dashboard and categories

--- dashboard.py
+++ dashboard.py
@@ -1,13 +1,53 @@
 import streamlit as st
 import pandas as pd
 import altair as alt
+import unicodedata
 
-# Define the path to the logos directory
-logos_path = "logos"
+# Lista de categorias válidas
+valid_categories = [
+    "DIVERSIDADE",
+    "ASSÉDIO(MORAL/SEXUAL)",
+    "MICROGERENCIAMENTO",
+    "SALÁRIO/BENEFÍCIOS",
+    "COMUNICAÇÃO/TRANSPARÊNCIA",
+    "POSSIBILIDADE DE CRESCIMENTO",
+    "SEGURANÇA/SAÚDE",
+    "FERRAMENTAS DE TRABALHO",
+    "LIDERANÇA TÓXICA",
+    "CARGA DE TRABALHO EXCESSIVA",
+]
+
+# Initialize session state
+if "empresa_selecionada" not in st.session_state:
+    st.session_state["empresa_selecionada"] = None
+
+# Load and process CSV file
+df = pd.read_csv(
+    "stream.csv",
+    usecols=[
+        "Nome da empresa",
+        "Denúncia",
+        "Em quais categorias sua denúncia se encaixa?",
+    ],
+)
+
+# Aplicar a remoção de acentos e conversão para minúsculas na coluna "Nome da empresa"
+df["Nome da empresa"] = (
+    df["Nome da empresa"]
+    .str.normalize("NFKD")
+    .str.encode("ascii", errors="ignore")
+    .str.decode("utf-8")
+)
+df["Nome da empresa"] = df["Nome da empresa"].str.lower()
+
+
+# Função para remover acentos de uma string
+def remover_acentos(txt):
+    return unicodedata.normalize("NFKD", txt).encode("ASCII", "ignore").decode("ASCII")
 
 
 # Function to normalize and count categories
-def normalize_and_count_categories(df, column_name):
+def normalize_and_count_categories(df, column_name, include_others=False):
     # Normalize and split categories
     df[column_name] = df[column_name].str.upper()  # Convert to uppercase
     df[column_name] = df[column_name].str.replace(
@@ -16,10 +56,17 @@ def normalize_and_count_categories(df, column_name):
     all_categories = df[column_name].str.split(",")  # Split categories
     all_categories = all_categories.explode()  # Transform list of categories into rows
     all_categories = all_categories.str.strip()  # Remove extra spaces
-    all_categories = all_categories[all_categories != "OUTROS"]  # Remove 'Outros'
-    # Count categories
+
+    if include_others:
+        # Filtrar somente as categorias válidas e contar, incluindo "OUTROS"
+        all_categories = all_categories.apply(
+            lambda x: x if x in valid_categories else "OUTROS"
+        )
+    else:
+        # Manter apenas as categorias que estão em valid_categories
+        all_categories = all_categories[all_categories.isin(valid_categories)]
+
     category_counts = all_categories.value_counts(normalize=True) * 100
-    # Sort categories from highest to lowest
     category_counts = category_counts.sort_values(ascending=False)
     return category_counts.reset_index(name="Value")
 
@@ -52,23 +99,24 @@ categorias_contagem = normalize_and_count_categories(
 # Calculate the top 5 most mentioned companies
 ranking_empresas = df["Nome da empresa"].value_counts().head(5)
 
+
 def reset_multiselect_companies():
     print("pediu pra limpar")
     st.session_state.multiselectcompanies = []
+
 
 # Left column: Top 5 most mentioned companies with generic logo
 with st.sidebar:
 
     # Create a borderless button that refreshes the page when clicked
-    if st.button("Início", type='primary'):
+    if st.button("Início", type="primary"):
         reset_multiselect_companies()
         st.session_state.empresa_selecionada = None
         st.experimental_rerun()
-
     st.title("Dashboard de Denúncias Tóxicas em Empresas")
     st.subheader("Top 5 Empresas Mais Citadas")
     for empresa in ranking_empresas.index:
-        if st.button(empresa, key=empresa): 
+        if st.button(empresa, key=empresa):
             reset_multiselect_companies()
             selecionar_empresa([empresa])
 
@@ -110,19 +158,24 @@ if st.session_state.empresa_selecionada:
     st.subheader(f"Denúncias da Empresa: {st.session_state.empresa_selecionada}")
     st.dataframe(df_empresa[["Denúncia"]], use_container_width=True, hide_index=True)
 else:
-    # Display the chart with categories for all companies
+    # Assegure-se de que esta linha seja executada antes de criar o gráfico inicial
+    categorias_contagem = normalize_and_count_categories(
+        df, "Em quais categorias sua denúncia se encaixa?"
+    )
+
+    # Agora, crie o gráfico de barras com esses dados processados
     st.subheader("Categorias de Denúncias (%)")
 
-    # Create the chart with the sorted DataFrame
     bar_chart = (
         alt.Chart(categorias_contagem)
         .mark_bar()
         .encode(
             x=alt.X(
                 "Em quais categorias sua denúncia se encaixa?:N",
-                sort=alt.EncodingSortField(field="Value", op="sum", order="ascending"),
-            ),
-            y=alt.Y("Value:Q"),
+                title="Categoria",
+                sort=alt.SortField(field="Value", order="descending"),
+            ),  # Certifique-se de que 'index' é o nome correto para a coluna de categorias
+            y=alt.Y("Value:Q", title="Porcentagem (%)"),
         )
     )
 
